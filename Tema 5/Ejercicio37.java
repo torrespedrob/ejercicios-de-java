b1aac9a1bf61ea95a2d86e0377e56abaf4486e2f
Pedro Torres Barba <torrespedrobarba@gmail.com>
2018-10-18 09:01:54 +0200

    Añadido un ejercicio nuevo

---
+++ Tema 5/Ejercicio37.java
@@ -0,0 +1,11 @@
+/**
+ * Ejercicio 38
+ * Pedro
+ */
+ import java.util.Scanner;
+ 
+public class Ejercicio38 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner (System.in);
+  }
+}
