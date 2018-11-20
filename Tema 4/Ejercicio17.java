f6982a6e1d173dcc41396a0ec7191bc07f356cd4
Pedro Torres Barba <43571231+torrespedrob@users.noreply.github.com>
2018-11-20 08:38:59 +0100

    Añadido un ejercicio anterior

---
+++ Tema 4/Ejercicio17.java
@@ -0,0 +1,21 @@
+/**
+ * Ejercicio 17
+ * Pedro
+ */
+
+public class Ejercicio17 {
+  public static void main(String[] args) {
+    System.out.println("Puedo decir la última cifra de un número entero introducido");
+    System.out.println("\nIntroduce un número entero");
+    
+    int x = Integer.parseInt(System.console().readLine());
+    System.out.println("La última cifra de ese número es: " + (x % 10));
+    
+  }
+}
+
+
+  
+
+  
+
